30d7ce44e693fddde5534cc219e66db694a1af97
shmelev <pavel.shmelev.v@gmail.com>
2013-01-10 19:37:53 +0400

    change location file

---
+++ src/CurrentTask.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author shmelev
+ */
+public class CurrentTask 
+{
+//TODO 
+    private static String[] strings=   {
+                "left in config file all options",
+                "Add config file reader",
+                "Status change fix(offline->invisible)"
+    
+    };
+    public static void main(String [] args)
+    {
+        for(String s: strings)
+        {
+            System.out.println(s);
+        }
+        
+    }
+}
